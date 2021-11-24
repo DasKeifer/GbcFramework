74e02fc8a4397c6599d3f6aaf9c76a27989cbf55
Das Keifer <EplanumProductions@gmail.com>
2021-11-24 16:06:44 -0500

    Initial commit

---
+++ src/gbc_framework/constants/RomConstants.java
@@ -0,0 +1,10 @@
+package gbc_framework.constants;
+
+
+public final class RomConstants 
+{
+	private RomConstants() {}
+	
+	public static final int BANK_SIZE = 0x4000;
+	public static final byte NUMBER_OF_BANKS = 64;
+}
