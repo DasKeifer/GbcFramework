845531b3e6cc710f988409d14b72fc4f8db83c57
Das Keifer <EplanumProductions@gmail.com>
2022-01-03 00:39:33 -0500

    Enhancements to the BankAddress class to use the new enums and patterns

--- src/gbc_framework/rom_addressing/BankAddress.java
+++ src/gbc_framework/rom_addressing/BankAddress.java
@@ -1,7 +1,10 @@
 package gbc_framework.rom_addressing;
 
+import com.sun.org.apache.xalan.internal.utils.XMLSecurityManager.Limit;
+
 import gbc_framework.RomConstants;
 import gbc_framework.utils.RomUtils;
+import sun.launcher.LauncherHelper;
 
 
 public class BankAddress 
@@ -11,6 +14,20 @@ public class BankAddress
 
 	public static final BankAddress UNASSIGNED = new BankAddress(UNASSIGNED_BANK, UNASSIGNED_ADDRESS);
 	public static final BankAddress ZERO = new BankAddress((byte) 0, (short) 0);
+
+	public enum BankAddressLimitType
+	{
+		IN_VALID_RANGES,
+		WITHIN_BANK,
+		WITHIN_BANK_OR_START_OF_NEXT
+	}
+
+	public enum BankAddressToUseType
+	{
+		BANK_AND_BANK_IN_ADDRESS,
+		BANK_ONLY,
+		ADDRESS_IN_BANK_ONLY
+	}
 	
 	private byte bank;
 	private short addressInBank;
@@ -51,50 +68,133 @@ public class BankAddress
 
 	public BankAddress newAtStartOfNextBank() 
 	{
-		if (isBankRange((byte) (bank + 1)))
+		if (isBankInRange((byte) (bank + 1)))
 		{
 			return new BankAddress((byte) (bank + 1), (short) 0);
 		}
 		return null;
 	}
 	
-	public BankAddress newOffsettedWithinBank(int offset)
+	public BankAddress newOffsetted(int offset)
 	{
-		if (!isAddressInBankRange(addressInBank + offset))
+		BankAddress copy = new BankAddress(this);
+		if (!copy.offset(offset))
 		{
 			return null;
 		}
-		return new BankAddress(bank, (short) (addressInBank + offset));
+		return copy;
 	}
-	
-	public enum LimitType
+
+	public BankAddress newOffsetted(int offset, BankAddressLimitType limit)
 	{
-		IN_VALID_RANGES,
-		WITHIN_BANK,
-		WITHIN_BANK_OR_START_OF_NEXT
-	}
-	
-	public enum ToUseType
-	{
-		BANK_AND_BANK_IN_ADDRESS,
-		BANK_ONLY,
-		ADDRESS_IN_BANK_ONLY
+		BankAddress copy = new BankAddress(this);
+		if (!copy.offset(offset, limit))
+		{
+			return null;
+		}
+		return copy;
 	}
 	
 	public BankAddress newSum(BankAddress toAdd)
 	{
-		return newSum(toAdd, ToUseType.BANK_AND_BANK_IN_ADDRESS, LimitType.IN_VALID_RANGES);
+		BankAddress copy = new BankAddress(this);
+		if (!copy.add(toAdd))
+		{
+			return null;
+		}
+		return copy;
 	}
 	
-	public BankAddress newSum(BankAddress toAdd, ToUseType whatToAdd)
+	public BankAddress newSum(BankAddress toAdd, BankAddressToUseType whatToAdd)
 	{
-		return newSum(toAdd, whatToAdd, LimitType.IN_VALID_RANGES);
+		BankAddress copy = new BankAddress(this);
+		if (!copy.add(toAdd, whatToAdd))
+		{
+			return null;
+		}
+		return copy;
 	}
 
-	// TODO: Add isFullAddress check to many of these?
-	public BankAddress newSum(BankAddress toAdd, ToUseType whatToAdd, LimitType limit)
+	public BankAddress newSum(BankAddress toAdd, BankAddressToUseType whatToAdd, BankAddressLimitType limit)
 	{
-		boolean valid = true;
+		BankAddress copy = new BankAddress(this);
+		if (!copy.add(toAdd, whatToAdd, limit))
+		{
+			return null;
+		}
+		return copy;
+	}
+	
+	public BankAddress newAbsoluteDifferenceBetween(BankAddress toGetRelativeOf)
+	{
+		return newAbsoluteDifferenceBetween(toGetRelativeOf, BankAddressToUseType.BANK_AND_BANK_IN_ADDRESS);
+	}
+	
+	public BankAddress newAbsoluteDifferenceBetween(BankAddress toGetRelativeOf, BankAddressToUseType whatToDiff)
+	{
+		switch (whatToDiff)
+		{
+		case BANK_ONLY:
+			return new BankAddress((byte) Math.abs(bank - toGetRelativeOf.bank), (short) 0);
+		case ADDRESS_IN_BANK_ONLY:
+			return new BankAddress((byte) 0, (short) Math.abs(addressInBank - toGetRelativeOf.addressInBank));
+		default: //Default to all
+		case BANK_AND_BANK_IN_ADDRESS:
+			return new BankAddress(Math.abs(getDifference(toGetRelativeOf, BankAddressToUseType.BANK_AND_BANK_IN_ADDRESS)));
+		}
+	}
+	
+	public boolean offset(int offset)
+	{
+		return offset(offset, BankAddressLimitType.IN_VALID_RANGES);
+	}
+	
+	public boolean offset(int offset, BankAddressLimitType limit)
+	{
+		byte newBank = bank;
+		short newAddress = (short) (addressInBank + offset);
+		switch (limit)
+		{
+		case WITHIN_BANK:
+		case WITHIN_BANK_OR_START_OF_NEXT:
+			if (!isAddressInBankInRange(bank, newAddress, limit))
+			{
+				return false;
+			}
+			break;
+		default: // Default to IN_VALID_RANGES
+		case IN_VALID_RANGES:
+			while (!isAddressInBankInRange(bank, newAddress, BankAddressLimitType.WITHIN_BANK))
+			{
+				newBank++;
+				newAddress -= RomConstants.BANK_SIZE;
+			}
+			
+			if (!isBankInRange(newBank))
+			{
+				return false;
+			}
+			break;
+		}
+
+		// Set the variables
+		bank = newBank;
+		addressInBank = newAddress;
+		return true;
+	}
+
+	public boolean add(BankAddress toAdd)
+	{
+		return add(toAdd, BankAddressToUseType.BANK_AND_BANK_IN_ADDRESS, BankAddressLimitType.IN_VALID_RANGES);
+	}
+	
+	public boolean add(BankAddress toAdd, BankAddressToUseType whatToAdd)
+	{
+		return add(toAdd, whatToAdd, BankAddressLimitType.IN_VALID_RANGES);
+	}
+	
+	public boolean add(BankAddress toAdd, BankAddressToUseType whatToAdd, BankAddressLimitType limit)
+	{
 		int bankSum = bank;
 		int addressSum = addressInBank;
 		switch(whatToAdd)
@@ -106,72 +206,43 @@ public class BankAddress
 			addressSum += toAdd.addressInBank;
 			break;
 		case BANK_AND_BANK_IN_ADDRESS:
-		default:
+		default: // Default to everything
 			bankSum += toAdd.bank;
 			addressSum += toAdd.addressInBank;
 			break;
 		}
 		
 		// Check the address
-		if (!isAddressInBankRange(addressSum))
-		{
-			// Switch behavior based on our limit
-			switch (limit)
-			{
-			case IN_VALID_RANGES:
-				bankSum++;
-				addressSum -= RomConstants.BANK_SIZE;
-				break;
-			case WITHIN_BANK_OR_START_OF_NEXT:
-				if (!isAddressInBankRange(addressSum - 1))
-				{
-					valid = false;
-				}
-				break;
-			case WITHIN_BANK:
-			default:
-				valid = false;
-				break;
-			}
-		}
+		boolean valid = isAddressInBankInRange(bankSum, addressSum, limit);
 		
 		// Now check bank if address is valid
 		if (valid)
 		{
 			switch (limit)
 			{
+			default: // Default to IN_VALID_RANGES
 			case IN_VALID_RANGES:
-				valid = isBankRange(bankSum);
+				valid = isBankInRange(bankSum);
 				break;
 			case WITHIN_BANK_OR_START_OF_NEXT:
 				valid = bankSum == bank ||
 					(bankSum - 1 == bank && addressSum == 0);
 				break;
 			case WITHIN_BANK:
-			default:
 				valid = bankSum == bank;
 				break;
 			}
 		}
 		
-		return valid ? new BankAddress((byte) bankSum, (short) addressSum) : null;
-	}
-	
-	public BankAddress newAbsoluteDifferenceBetween(BankAddress toGetRelativeOf)
-	{
-		return new BankAddress(Math.abs(getDifference(toGetRelativeOf)));
-	}
-	
-	public boolean offsetWithinBank(int offset)
-	{
-		if (!isAddressInBankRange(addressInBank + offset))
+		if (valid)
 		{
-			return false;
+			bank = (byte) bankSum;
+			addressInBank = (short) addressSum;
+			return true;
 		}
-		addressInBank += offset;
-		return true;
+		return false;
 	}
-
+	
 	public void setToCopyOf(BankAddress toCopy) 
 	{
 		bank = toCopy.bank;
@@ -180,7 +251,7 @@ public class BankAddress
 
 	public void setBank(byte bank) 
 	{
-		if (!isBankRange(bank))
+		if (!isBankInRange(bank))
 		{
 			throw new IllegalArgumentException("BankAddress - invalid bank given (" + bank + 
 					") - the bank must be between 0 and " + RomConstants.NUMBER_OF_BANKS + 
@@ -191,7 +262,7 @@ public class BankAddress
 	
 	public void setAddressInBank(short addressInBank) 
 	{
-		if (!isAddressInBankRange(addressInBank))
+		if (!isAddressInBankInRange(bank, addressInBank, BankAddressLimitType.WITHIN_BANK))
 		{
 			throw new IllegalArgumentException("BankAddress - invalid addressInBank given (" + addressInBank + 
 					") - the bank must be between 0 and " + RomConstants.BANK_SIZE +
@@ -200,18 +271,33 @@ public class BankAddress
 		this.addressInBank = addressInBank;
 	}
 	
-	private boolean isBankRange(int toCheck)
+	private static boolean isBankInRange(int bankToCheck)
 	{
-		return (toCheck >= 0 && toCheck < RomConstants.NUMBER_OF_BANKS) ||
-				toCheck == UNASSIGNED_BANK;
+		return (bankToCheck >= 0 && bankToCheck < RomConstants.NUMBER_OF_BANKS) ||
+				bankToCheck == UNASSIGNED_BANK;
 	}
 	
-	private boolean isAddressInBankRange(int toCheck)
+	private static boolean isAddressInBankInRange(int bankToCheck, int addrToCheck, BankAddressLimitType type)
 	{
-		return (toCheck >= 0 && toCheck < RomConstants.BANK_SIZE) ||
-				toCheck == UNASSIGNED_ADDRESS;
+		switch (type)
+		{
+		default:
+		case IN_VALID_RANGES:
+			while (!isAddressInBankInRange(bankToCheck, addrToCheck, BankAddressLimitType.WITHIN_BANK))
+			{
+				bankToCheck++;
+				addrToCheck -= RomConstants.BANK_SIZE;
+			}
+			return isBankInRange(bankToCheck);
+		case WITHIN_BANK:
+		case WITHIN_BANK_OR_START_OF_NEXT:
+			return (addrToCheck == UNASSIGNED_ADDRESS ||
+					addrToCheck >= 0 && 
+					(addrToCheck < RomConstants.BANK_SIZE || 
+							(type ==  BankAddressLimitType.WITHIN_BANK_OR_START_OF_NEXT && addrToCheck == RomConstants.BANK_SIZE)));
+		}
 	}
-
+	
 	public boolean isFullAddress() 
 	{
 		return bank != UNASSIGNED_BANK && addressInBank != UNASSIGNED_ADDRESS;
@@ -232,19 +318,14 @@ public class BankAddress
 		return bank == toCheck.bank;
 	}
 
-	public boolean fitsInBankAddressWithOffset(int offset) 
+	public boolean offsetFits(int offset) 
 	{
-		// Can't check if this address isn't assigned yet
-		if (addressInBank == UNASSIGNED_ADDRESS)
-		{
-			return false;
-		}
-		
-		// -1 to make the check "inclusive"
-		// i.e. if the address is the last address in the bank, it can still fit
-		// one byte at that address. If its the first address in the bank, then
-		// it can fit the whole bank size
-		return isAddressInBankRange(addressInBank + offset - 1);
+		return isAddressInBankInRange(bank, this.addressInBank + offset, BankAddressLimitType.IN_VALID_RANGES);
+	}
+	
+	public boolean offsetFits(int offset, BankAddressLimitType type) 
+	{
+		return isAddressInBankInRange(bank, this.addressInBank + offset, type);
 	}
 
 	public byte getBank()
@@ -259,14 +340,51 @@ public class BankAddress
 	
 	public int getDifference(BankAddress other)
 	{
-		return (other.bank - bank) * RomConstants.BANK_SIZE +
-				other.addressInBank - addressInBank;
+		return getDifference(other, BankAddressToUseType.BANK_AND_BANK_IN_ADDRESS);
 	}
 	
+	public int getDifference(BankAddress other, BankAddressToUseType whatToDiff)
+	{
+		switch (whatToDiff)
+		{
+		default: // Default to all
+		case BANK_AND_BANK_IN_ADDRESS:
+			return (other.bank - bank) * RomConstants.BANK_SIZE +
+					other.addressInBank - addressInBank;
+		case BANK_ONLY:
+			return (other.bank - bank) * RomConstants.BANK_SIZE;
+		case ADDRESS_IN_BANK_ONLY:
+			return other.addressInBank - addressInBank;
+		}
+	}
+
 	public AddressRange getDifferenceAsRange(BankAddress other)
 	{
-		int globalAddress = RomUtils.convertToGlobalAddress(bank, addressInBank);
-		int otherGlobalAddress = RomUtils.convertToGlobalAddress(other.bank, other.addressInBank);
+		return getDifferenceAsRange(other, BankAddressToUseType.BANK_AND_BANK_IN_ADDRESS);
+	}
+	
+	public AddressRange getDifferenceAsRange(BankAddress other, BankAddressToUseType whatToDiff)
+	{
+		int globalAddress = 0;
+		int otherGlobalAddress = 0;
+		
+		switch (whatToDiff)
+		{
+		default: // Default to both
+		case BANK_AND_BANK_IN_ADDRESS:
+			globalAddress = RomUtils.convertToGlobalAddress(bank, addressInBank);
+			otherGlobalAddress = RomUtils.convertToGlobalAddress(other.bank, other.addressInBank);
+			break;
+		case BANK_ONLY:
+			globalAddress = RomUtils.convertToGlobalAddress(bank, (short) 0);
+			otherGlobalAddress = RomUtils.convertToGlobalAddress(other.bank, (short) 0);
+			break;
+		case ADDRESS_IN_BANK_ONLY:		
+			globalAddress = addressInBank;
+			otherGlobalAddress = other.addressInBank;
+			break;
+		}
+		
 		if (globalAddress > otherGlobalAddress)
 		{
 			return new AddressRange(otherGlobalAddress, globalAddress);
@@ -282,6 +400,11 @@ public class BankAddress
     @Override
     public boolean equals(Object o) 
     {
+    	return equals(o, BankAddressToUseType.BANK_AND_BANK_IN_ADDRESS);
+    }
+    
+    public boolean equals(Object o, BankAddressToUseType whatToCheck)
+    {
         // If the object is compared with itself then return true 
         if (o == this) {
             return true;
@@ -295,7 +418,17 @@ public class BankAddress
         
         // Compare the data and return accordingly
         BankAddress ba = (BankAddress) o;
-        return Byte.compare(bank, ba.bank) == 0 && Short.compare(addressInBank, ba.addressInBank) == 0;
+        
+        switch (whatToCheck)
+        {
+        default:
+        case BANK_AND_BANK_IN_ADDRESS:
+            return Byte.compare(bank, ba.bank) == 0 && Short.compare(addressInBank, ba.addressInBank) == 0;
+        case BANK_ONLY:
+            return Byte.compare(bank, ba.bank) == 0;
+    	case ADDRESS_IN_BANK_ONLY:
+    		return Short.compare(addressInBank, ba.addressInBank) == 0;
+        }
     }
     
     @Override
